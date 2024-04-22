84b04ac513e57aa1f729a4c6de6d6a71435fb393
m-usman98 <77602134+m-usman98@users.noreply.github.com>
2024-04-22 09:01:53 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -9,10 +9,11 @@ class load_dataset(torch.utils.data.Dataset):
     def __init__(self, dataset_path, transform, IMG_HEIGHT, IMG_WIDTH):
         self.IMG_HEIGHT = IMG_HEIGHT
         self.IMG_WIDTH = IMG_WIDTH
-        self.df = pd.read_csv(os.path.join(dataset_path, 'labels.csv'), sep=',', engine='python', encoding="ISO-8859-1")
+        self.df = pd.read_csv(os.path.join(dataset_path, 'labels.csv'), sep=',', engine='python', encoding="utf-8")
         self.images_folder = dataset_path
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Resize((self.IMG_HEIGHT, self.IMG_WIDTH)),
-                                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self.transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Resize((self.IMG_HEIGHT, self.IMG_WIDTH)),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
     def __len__(self):
         return len(self.df)
@@ -24,3 +25,27 @@ class load_dataset(torch.utils.data.Dataset):
         if self.transform is not None:
             image = self.transform(image)
         return image, label
+
+
+class load_test_dataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_path, transform, IMG_HEIGHT, IMG_WIDTH):
+        self.IMG_HEIGHT = IMG_HEIGHT
+        self.IMG_WIDTH = IMG_WIDTH
+        self.IMG_PTH = []
+        for i in os.listdir(dataset_path):
+            self.IMG_PTH.append(i)
+
+        self.images_folder = dataset_path
+        self.transform = transforms.Compose(
+            [transforms.ToTensor(), transforms.Resize((self.IMG_HEIGHT, self.IMG_WIDTH)),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    def __len__(self):
+        return len(self.IMG_PTH)
+
+    def __getitem__(self, index):
+        filename = self.IMG_PTH[index]
+        image = Image.open(filename)
+        if self.transform is not None:
+            image = self.transform(image)
+        return image
